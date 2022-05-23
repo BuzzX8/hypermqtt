acdca0167a498e47e9b9ad30eb68f518e4b84846
Maxim <maxim.chasnik@gmail.com>
2022-05-23 20:17:41 +0300

    wip

---
+++ src/packet.rs
@@ -0,0 +1,17 @@
+pub enum Packet {
+    Connect,
+    ConnAck,
+    Publish,
+    PubAck,
+    PubRec,
+    PubRel,
+    PubComp,
+    Subscribe,
+    SubAck,
+    Unsubscribe,
+    UnsubAck,
+    PingReq,
+    PingResp,
+    Disconnect,
+    Auth
+}
